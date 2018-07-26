ba6fe48b4e4b93b5e47128cb070e8e9caca2309e
Mike White <mike.wildcats@gmail.com>
2018-07-26 14:23:19 -0400

    Data models complete
    	modified:   .gitignore
    	new file:   InfraMgmt/Data/InfraDbContext.cs
    	new file:   InfraMgmt/Models/Asset.cs
    	new file:   InfraMgmt/Models/Category.cs
    	new file:   InfraMgmt/Models/Manufacturer.cs

---
+++ InfraMgmt/Data/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfraMgmt.Data
+{
+    public class Config
+    {
+        public static string Connection = "server=localhost;database=inventory;user=root;password=Ljtmacl1$$$";
+    }
+}
---
+++ InfraMgmt/Models/Asset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfraMgmt.Models
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+        public int AssetId { get; set; }
+        public string Serial { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public string Status { get; set; }
+        public string Location { get; set; }
+        public string Category { get; set; }
+        public DateTime CheckoutDate { get; set; }
+        public DateTime CheckinDate { get; set; }
+    }
+}
