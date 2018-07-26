87a4aa5ca2a533a0bec601dc8e47899caf82de74
Mike White <mike.wildcats@gmail.com>
2018-07-26 14:31:37 -0400

    Config file moved

--- InfraMgmt/Data/InfraDbContext.cs
+++ InfraMgmt/Data/InfraDbContext.cs
@@ -3,10 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InfraMgmt.Models;
 
 namespace InfraMgmt.Data
 {
     public class InfraDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL()
+        }
     }
 }
